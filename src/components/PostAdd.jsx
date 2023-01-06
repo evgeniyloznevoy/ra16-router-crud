3a28cf319b025a11f622a7bdf83d4799b91b53d5
Evgeniy Loznevoy <evgeniy.loznevoy@yandex.ru>
2023-01-06 13:30:22 +0300

    fix 06.01

--- src/components/PostAdd.jsx
+++ src/components/PostAdd.jsx
@@ -1,28 +1,3 @@
-// import React, {useState} from "react";
-// import {NavLink} from "react-router-dom";
-
-// export default function PostAdd (props) {
-//     const [value, setValue] = useState('');
-
-//     const onSubmitForm = (e) => {
-//         e.preventDefault();
-//         props.addPost(value);
-//         props.history.push('/');
-//     }
-//     return (
-//         <form onSubmit={onSubmitForm} className='postAddForm'>
-//             <button className='close-form'><NavLink to='/'>&#10006;</NavLink></button>
-//             <textarea
-//                 placeholder='О чем Вы сейчас думаете?'
-//                 onChange={(e) => {setValue(e.target.value);}}
-//             />
-//             <button type='submit' className='postAddForm-btn'>Опубликовать</button>
-//         </form>
-//     )
-
-// }
-
-
 import { Link } from "react-router-dom";
 
 function PostAdd() {
